41f86ba2e3775b1435633d7df30a753f6e8c968f
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-04-15 10:47:53 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const QR = require("./Controllers/qrgenerator");
+const InventryMaster = require("./Controllers/Inventry");
+
+//QR 
+router.get("/", QR.myFunc);
+router.get("/getQRlist", QR.getQRData);
+router.get("/getQRlistbyItemNumber", QR.getQRlistbyItemNumberData);
+
+//Inventry 
+router.get("/inventryList", InventryMaster.getInventryList);
+router.post("/inventryList", InventryMaster.addInventryData);
+router.put("/inventryList", InventryMaster.updateInventryData);
+router.delete("/inventryList", InventryMaster.deleteInventryData);
+
+
+module.exports = router;
